d30cb9bfdb87acb6fb9cf34d56691f171d82f8bb
gbulmersa <glen.bulmer@stateauto.com>
2017-03-29 16:06:54 -0400

    Added first few tests

---
+++ src/com/stateauto/bowling/BowlingGame.java
@@ -0,0 +1,25 @@
+package com.stateauto.bowling;
+
+/**
+ * Created by bul2852 on 3/29/2017.
+ */
+public class BowlingGame {
+    public int score() {
+        return 0;
+    }
+
+    public void roll() {
+    }
+
+    public String[] rollOneBall(int i) {
+        return new String[]{"0"};
+    }
+
+    public String[] rollAllGutterBalls() {
+        return new String[]{"0","0","0","0","0","0","0","0","0","0"};
+    }
+
+    public String[] onePinPerRoll() {
+        return new String[]{"2","4","6","8","10","12","14","16","18","20"};
+    }
+}
--- test/com/stateauto/test/bowlingTest.java
+++ test/com/stateauto/test/bowlingTest.java
@@ -1,14 +1,59 @@
 package com.stateauto.test;
 
 
+import com.stateauto.bowling.BowlingGame;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by bul2852 on 3/29/2017.
  */
 public class bowlingTest {
-    @Test
-    public void nothingTest() {
 
+    private BowlingGame bg;
+    private void printGame(String gameTitle, String[] frames) {
+        System.out.println(gameTitle);
+        for (int i=0; i<frames.length; i++) {
+            System.out.println("Frame " + String.valueOf(i+1) + ": " + frames[i]);
+        }
+        System.out.println();
     }
+
+    @Before
+    public void setUp() throws Exception {
+        bg = new BowlingGame();
+    }
+
+    @Test
+    public void canCreateBowlingGameTest() {
+        assertTrue(bg != null);
+    }
+
+    @Test
+    public void rollOneGutterBallTest() throws Exception {
+        String ret[] = bg.rollOneBall(0);
+        assertEquals("0", ret[0]);
+        printGame("Roll one gutter ball test",ret );
+    }
+
+    @Test
+    public void rollAllGutterBallsTest() throws Exception {
+        String[] ret = bg.rollAllGutterBalls();
+        assertTrue(ret.length == 10);
+        printGame("All gutter balls test", ret);
+    }
+
+    @Test
+    public void onePinPerRollTest() throws Exception {
+        String[] ret =  bg.onePinPerRoll();
+        assertTrue(ret.length == 10);
+        assertEquals("20", ret[9]);
+        printGame("One pin per roll test", ret);
+    }
+
+    
 }
