e0147f2cb42d26906e323d53ec7deb5eaf57538f
gbulmersa <glen.bulmer@stateauto.com>
2017-03-29 14:43:52 -0400

    created project files

---
+++ test/com/stateauto/test/bowlingTest.java
@@ -0,0 +1,14 @@
+package com.stateauto.test;
+
+
+import org.junit.Test;
+
+/**
+ * Created by bul2852 on 3/29/2017.
+ */
+public class bowlingTest {
+    @Test
+    public void nothingTest() {
+
+    }
+}
